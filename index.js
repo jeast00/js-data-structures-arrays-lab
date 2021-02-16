5bd79febb2ebbeea90ad370a8dca1dd5743abc72
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-02-16 13:47:12 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    const driver = [...drivers, name];
+    return driver;
+}
+
+function prependDriver(name) {
+    const driver = [name, ...drivers];
+    return driver;
+}
+
+function removeLastDriver() {
+    const driver = drivers.slice(0, -1);
+    return driver;
+}
+
+function removeFirstDriver() {
+    const driver = drivers.slice(1);
+    return driver;
+}
